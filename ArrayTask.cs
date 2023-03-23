1666882d85a180f3a8c652932888235329f21793
RiyazurRazak <riyazurrazaknoushath786@gmail.com>
2023-03-23 16:46:08 +0530

    files added/updated 23 March AN-II session

---
+++ ArrayTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace week_1
+{
+    internal class ArrayTask
+    {
+        static void Main(string[] args)
+        {
+
+            // first question
+
+            int[] duplicateValueArray = { 1, 1, 2, 3, 4, 5, 6, 4, 2, 56, 45, 3 };
+            HashSet<int> distinctContainer = new HashSet<int>();
+            foreach (int value in duplicateValueArray)
+            {
+                distinctContainer.Add(value);
+            }
+            int totalDuplicateElements = duplicateValueArray.Length - distinctContainer.Count;
+            Console.WriteLine("Total duplicate values are: " +totalDuplicateElements);
+
+            // Second Question
+
+            const int maxRange = 10;
+            List<int> oddNumbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
+            for(int number=1; number<=maxRange; number++)
+            {
+                if(number % 2 == 0) evenNumbers.Add(number);
+                else oddNumbers.Add(number);
+            }
+            Console.WriteLine("Odd Numbers are :");
+            foreach(int number in oddNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Even Numbers are :");
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace week_1
+{
+    internal class Game
+    {
+        const int bulletsCount = 20;
+        public int heroFires = 0;
+        public int villanFires = 0;
+        bool isVillanBulletsOver = false;
+
+        public void Shoot()
+        {
+            if(heroFires <= bulletsCount)
+            {
+                heroFires++;
+            }
+            if (villanFires + 3 < bulletsCount && !isVillanBulletsOver)
+            {
+                villanFires += 3;
+
+            }
+            else
+            {
+                isVillanBulletsOver = true;
+                villanFires = 0;
+            }
+            Console.WriteLine("Hero Fires : " + heroFires + " Villan Fires :" + villanFires);
+        }
+        // renamed the Main to main to run other files in the projects!
+        static void main(string[] args)
+        {
+            Game game = new Game();
+            int itr = 0;
+            while (itr != 20)
+            {
+                game.Shoot();
+                itr++;
+            }
+           
+
+        }
+    }
+}
